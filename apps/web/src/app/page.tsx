3dacd6ceb0deff5f90c31e9b9db5b1c16d09abd0
Thea Kolnes <thea.jenny02@gmail.com>
2023-10-10 01:37:59 +0200

    import images from sanity

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,26 +1,26 @@
-import Image from 'next/image'
+import { getPicture } from '@/lib/picture';
+import { Picture } from '@/lib/picture/schemas';
+import { urlFor } from '@/utils/image-builder';
+import Image from 'next/image';
 
-export default function Home() {
+export default async function Home() {
+  const data = await getPicture() as Picture[];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-xl w-full mb-10 items-center justify-between font-mono text-sm lg:flex">
+    <main className='flex min-h-screen flex-col items-center p-24'>
+      <div className='max-w-2xl w-full justify-between font-mono text-sm m-6'>
         This is my photography gallery.
       </div>
 
-      <div className="columns-xs gap-4 space-y-4 w-4/5" m-2>
-        <img className="w-full aspect-picture" src='https://i.etsystatic.com/7680109/r/il/39ccd0/3370717066/il_1588xN.3370717066_bcz3.jpg'/>
-        <img className="w-full aspect-picture" src='https://i.pinimg.com/originals/c5/ab/aa/c5abaacf2eb008b28c9b6ca1ae442ed6.png'/>
-        <img className="w-full aspect-picture" src='https://i.etsystatic.com/12920008/r/il/cb7a33/3757503860/il_1588xN.3757503860_3n41.jpg'/>
-        <img className="w-full aspect-picture" src='https://i.etsystatic.com/7680109/r/il/39ccd0/3370717066/il_1588xN.3370717066_bcz3.jpg'/>
-        <img className="w-full aspect-picture" src='https://i.etsystatic.com/12920008/r/il/cb7a33/3757503860/il_1588xN.3757503860_3n41.jpg'/>
-        <img className="w-full aspect-picture" src='https://i.etsystatic.com/7680109/r/il/39ccd0/3370717066/il_1588xN.3370717066_bcz3.jpg'/>
-        <img className="w-full aspect-picture" src='https://i.pinimg.com/originals/c5/ab/aa/c5abaacf2eb008b28c9b6ca1ae442ed6.png'/>
-        <img className="w-full aspect-picture" src='https://i.etsystatic.com/12920008/r/il/cb7a33/3757503860/il_1588xN.3757503860_3n41.jpg'/>
-        <img className="w-full aspect-picture" src='https://i.etsystatic.com/7680109/r/il/39ccd0/3370717066/il_1588xN.3370717066_bcz3.jpg'/>
-        <img className="w-full aspect-picture" src='https://i.pinimg.com/originals/c5/ab/aa/c5abaacf2eb008b28c9b6ca1ae442ed6.png'/>
-        <img className="w-full aspect-picture" src='https://i.pinimg.com/originals/c5/ab/aa/c5abaacf2eb008b28c9b6ca1ae442ed6.png'/>
-        <img className="w-full aspect-picture" src='https://i.etsystatic.com/12920008/r/il/cb7a33/3757503860/il_1588xN.3757503860_3n41.jpg'/>
+      <div className='columns-xs w-5/6 gap-4'>
+        <ul>
+          {data.map((picture) => (
+            <li key={picture._id} className='mb-4'>
+              <Image className='w-full aspect-picture' alt={picture.name} src={urlFor(picture.image).width(1000).url()} width={200} height={200} />
+            </li>
+          ))}
+        </ul>
       </div>
     </main>
-  )
+  );
 }
