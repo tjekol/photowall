dbd20737c7bd5a23b40791358b77414e3ece5533
Thea Kolnes <thea.jenny02@gmail.com>
2023-10-10 12:28:27 +0200

    updated README.md and added description

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -7,11 +7,14 @@ export default async function Home() {
   const data = await getPicture() as Picture[];
 
   return (
-    <main className='flex min-h-screen flex-col items-center p-24'>
+    <main className='flex min-h-screen static flex-col items-center p-24'>
       <div className='max-w-2xl w-full justify-between font-mono text-sm m-6'>
         <h1 className='text-lg'>
           This is my photography gallery.
         </h1>
+        <p className='text-gray-600'>
+          This website are photos taken with my camera. They are uploaded through <a href='https://www.sanity.io' target='_' className='font-semibold hover:underline'>Sanity</a> and fetched with Next.js.
+        </p>
       </div>
 
       <div className='columns-xs w-5/6 gap-4'>
@@ -23,7 +26,7 @@ export default async function Home() {
           ))}
         </ul>
       </div>
-      <a href='https://github.com/tjekol/memories' target='_' className='absolute bottom-6 hover:underline decoration-solid text-gray-500'>Made by Thea Jenny Kolnes</a>
+      <a href='https://github.com/tjekol/memories' target='_' className='static bottom-6 mt-8 hover:underline text-gray-400'>Made by Thea Jenny Kolnes🦋</a>
     </main>
   );
 }
