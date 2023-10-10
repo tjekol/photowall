e4d2606159df11a01c5738e50c2ec1d32665dc32
Thea Kolnes <thea.jenny02@gmail.com>
2023-10-10 17:49:10 +0200

    resized page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -7,26 +7,33 @@ export default async function Home() {
   const data = await getPicture() as Picture[];
 
   return (
-    <main className='flex min-h-screen static flex-col items-center p-24'>
+    <main className='flex flex-col min-h-screen  items-center p-24'>
       <div className='max-w-2xl w-full justify-between font-mono text-sm m-6'>
-        <h1 className='text-lg'>
+        <h1 className='text-2xl'>
           This is my photography gallery.
         </h1>
-        <p className='text-slate-400'>
+        <p className='text-slate-400 mb-6'>
           This website is a place where I can store my photos taken with my camera. They are uploaded through <a href='https://www.sanity.io' target='_' className='font-semibold hover:underline'>Sanity</a>.
         </p>
       </div>
 
-      <div className='columns-xs w-5/6 gap-4'>
+      <div className='columns-xs w-4/5 gap-4'>
         <ul>
           {data.map((picture) => (
             <li key={picture._id} className='mb-4'>
-              <Image className='w-full aspect-picture' alt={picture.name} src={urlFor(picture.image).width(1000).url()} width={200} height={200} />
+              <Image
+                className='w-full aspect-picture'
+                alt={picture.name}
+                src={urlFor(picture.image).width(1000).url()}
+                width={200}
+                height={200}
+              />
             </li>
           ))}
         </ul>
       </div>
       <a href='https://github.com/tjekol/memories' target='_' className='static bottom-6 mt-8 hover:underline text-gray-400'>Made by Thea Jenny Kolnes🦋</a>
     </main>
+    
   );
 }
