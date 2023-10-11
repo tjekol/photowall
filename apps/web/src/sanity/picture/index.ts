9063f699843dcd9a6588d042f65cd54fdcbc0f45
Thea Kolnes <thea.jenny02@gmail.com>
2023-10-11 23:32:58 +0200

    fixed directories + layout

--- apps/web/src/sanity/picture/index.ts
+++ apps/web/src/sanity/picture/index.ts
@@ -2,7 +2,7 @@ import { Picture } from './schemas';
 import { groq } from 'next-sanity';
 
 export async function getPicture(): Promise<Picture[]> {
-  const { client } = await import('@/lib/sanity');
+  const { client } = await import('@/sanity/client');
   
   return client.fetch(
     groq`*[_type == "picture"] | order(_createdAt desc){
