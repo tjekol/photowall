b401b4d2a5d988a75aa8a174f0a52e0e498baa2f
Thea Kolnes <thea.jenny02@gmail.com>
2023-10-18 20:22:07 +0200

    testing rebase

--- apps/web/src/app/api/picture/route.ts
+++ apps/web/src/app/api/picture/route.ts
@@ -1,8 +1,25 @@
-import {NextResponse} from 'next/server';
+// 97u2fmo5.api.sanity.io requests
+// 97u2fmo5.apicdn.sanity.io api CDN cache endpoint
 
-export function GET(request: Request) {
+// API: https://97u2fmo5.api.sanity.io/v2023-10-09/data/query/production
+// API CDN: https://97u2fmo5.apicdn.sanity.io/v2023-10-09/data/query/production
 
-  return NextResponse.json({
-    message: 'i am trying to send a fucking image', request,
-  });
-}
+// https://97u2fmo5.api.sanity.io/v2023-10-09/data/query/production?query=*[_type=="picture"]|order(_createdAt%20desc)
+// https://97u2fmo5.api.sanity.io/v2023-10-09/data/query/production?query=*[_type=="picture"]|order(_createdAt%20desc)
+
+
+
+import { NextResponse } from 'next/server';
+
+const PICTURE_API_URL = 'https://memories-tjekol.vercel.app/api/picture';
+
+fetch(PICTURE_API_URL)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+
+// export function GET(request: Request) {
+//   return NextResponse.json({
+//     message: 'i am trying to send a fucking image', request,
+//   });
+// }
