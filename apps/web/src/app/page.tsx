e112f18ae9b6e938dfbab2d4d7f3e94cc3f09596
Thea Kolnes <thea.jenny02@gmail.com>
2023-11-01 22:51:19 +0100

    padding and text size

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -17,17 +17,17 @@ export default function HomePage() {
   }, [pictures]);
 
   return (
-    <main className='flex flex-col min-h-screen items-center p-24'>
+    <main className='flex flex-col min-h-screen items-center p-10 sm:p-24'>
       <div className='max-w-2xl w-full justify-between font-mono text-sm m-6'>
-        <h1 className='text-2xl'>
+        <h1 className='text-lg sm:text-2xl'>
           This is my photography gallery.
         </h1>
-        <p className='text-slate-400 mb-6'>
+        <p className=' text-sm sm:text-lg text-slate-400 mb-6'>
           This website is a place where I can store my photos taken with my camera. They are uploaded through <a href='https://www.sanity.io' target='_' className='font-semibold hover:underline'>Sanity</a>.
         </p>
       </div>
 
-      <div className='columns-xs w-4/5 gap-4'>
+      <div className='columns-xs w-full sm:w-4/5 gap-4 '>
         {pictures.map((picture) => (
           <div key={picture._id} className='mb-4'>
             <Image className='w-full aspect-image' src={picture.image} alt={picture.name} width={350} height={200}/>
