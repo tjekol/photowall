076bf1327c9a1283258857c7681c3ccdb0d30c68
Thea Jenny E Kolnes <thea.jenny02@gmail.com>
2026-06-27 17:46:03 +0200

    style: new landing page

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -14,7 +14,14 @@ export default function DefaultLayout({
 }) {
   return (
     <html lang='en' className='light' data-theme='light'>
-      <body className='bg-background text-foreground'>{children}</body>
+      <body className='bg-background text-foreground'>
+        <main className='flex flex-col min-h-screen items-center p-10'>
+          <h1 className='font-semi-bold m-2'>
+            <a href='/'>Photo Wall</a>
+          </h1>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import { getPicture } from '@/sanity/picture';
 import { IPicture } from '@/sanity/picture/schemas';
 import Image from 'next/image';
+import { Button } from '@heroui/react';
 
 export default function HomePage() {
   const [pictures, setPictures] = useState<IPicture[]>([]);
@@ -17,25 +18,31 @@ export default function HomePage() {
   }, [pictures]);
 
   return (
-    <main className='flex flex-col min-h-screen items-center p-10 sm:p-24'>
-      <div className='max-w-2xl w-full justify-between font-mono text-sm m-6'>
-        <h1 className='text-lg sm:text-2xl'>
-          This is my photography gallery.
-        </h1>
-        <p className=' text-sm sm:text-lg text-slate-400 mb-6'>
-          This website is a place where I can store my photos taken with my camera. They are uploaded through <a href='https://www.sanity.io' target='_' className='font-semibold hover:underline'>Sanity</a>.
-        </p>
-      </div>
+    <>
+      <Button className='mb-4'>
+        <a href='add-photo'>Share a photo!</a>
+      </Button>
 
       <div className='columns-xs w-full sm:w-4/5 gap-4 '>
         {pictures.map((picture) => (
           <div key={picture._id} className='mb-4'>
-            <Image className='w-full aspect-image' src={picture.image} alt={picture.name} width={350} height={200}/>
+            <Image
+              className='w-full aspect-image'
+              src={picture.image}
+              alt={picture.name}
+              width={350}
+              height={200}
+            />
           </div>
         ))}
       </div>
-      <a href='https://github.com/tjekol/memories' target='_' className='static bottom-6 mt-8 hover:underline text-gray-400'>Made by Thea Jenny Kolnes🦋</a>
-    </main>
-    
+      <a
+        href='https://github.com/tjekol/memories'
+        target='_'
+        className='static bottom-6 mt-8 hover:underline text-gray-400'
+      >
+        Made by TJEKOL🦋
+      </a>
+    </>
   );
 }
