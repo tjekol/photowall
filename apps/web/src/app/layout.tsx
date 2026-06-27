b9409f8929ef7d9e9d59ea6b2ea2121ae34d822f
Thea Jenny E Kolnes <thea.jenny02@gmail.com>
2026-06-27 17:43:49 +0200

    style: updated css + layout

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -10,11 +10,11 @@ export const metadata: Metadata = {
 export default function DefaultLayout({
   children,
 }: {
-  children: ReactNode;
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html lang='en' className='light' data-theme='light'>
+      <body className='bg-background text-foreground'>{children}</body>
     </html>
   );
 }
